2cb684dd7801419d35f30fae1e1b70dd8b4ebc75
febdanielh <febriandanielh09@gmail.com>
2023-09-27 17:10:32 +0700

    Initial commit

---
+++ DollarBills/DollarBillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarBillsApp.swift
+//  DollarBills
+//
+//  Created by Febrian Daniel on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DollarBillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
